0ed7841e43ebfa80cf1ca8f2a5ffcfc529ba758b
Bin Liu <bliu@suse.com>
2017-08-24 15:19:26 +0800

    json

---
+++ python/json1.py
@@ -0,0 +1,25 @@
+from io import StringIO
+import json
+ 
+fileObj = StringIO()
+json.dump(["Hello", "Geeks"], fileObj)
+print("Using json.dump(): "+str(fileObj.getvalue()))
+ 
+class TypeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, type):
+            return str(obj)
+ 
+print("Using json.dumps(): "+str(json.dumps(type(str), cls=TypeEncoder)))
+print("Using json.JSONEncoder().encode"+
+      str(TypeEncoder().encode(type(list))))
+print("Using json.JSONEncoder().iterencode"+
+      str(list(TypeEncoder().iterencode(type(dict)))))
+
+fileObj = StringIO('["Geeks for Geeks"]')
+print("Using json.load(): "+str(json.load(fileObj)))
+print("Using json.loads(): "+str(json.loads('{"Geeks": 1, "for": 2, "Geeks": 3}')))
+print("Using json.JSONDecoder().decode(): " +
+    str(json.JSONDecoder().decode('{"Geeks": 1, "for": 2, "Geeks": 3}')))
+print("Using json.JSONDecoder().raw_decode(): " +
+    str(json.JSONDecoder().raw_decode('{"Geeks": 1, "for": 2, "Geeks": 3}')))
