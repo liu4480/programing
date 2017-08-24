61667e22e06ab38251f8dbe6d4d4dc92f2080aa7
Bin Liu <bliu@suse.com>
2017-08-24 16:00:01 +0800

    input methods for competitive programing

---
+++ python/input-Methods-for-Competitive-Programming.py
@@ -0,0 +1,46 @@
+n = int(raw_input())
+ 
+# input the array
+arr = [int(x) for x in raw_input().split()]
+ 
+# initialize variable
+summation = 0
+ 
+# calculate sum
+for x in arr:
+    summation += x
+     
+# print answer
+print(summation)
+
+from sys import stdin, stdout 
+ 
+# suppose a function called main() and
+# all the operations are performed
+def main():
+ 
+    # input via readline method
+    n = stdin.readline()
+ 
+    # array input similar method
+    arr = [int(x) for x in stdin.readline().split()]
+ 
+    #initialize variable
+    summation = 0
+     
+    # calculate sum
+    for x in arr:
+        summation += x
+ 
+    # could use inbuilt summation = sum(arr)
+ 
+    # print answer via write
+    # write method writes only
+    # string operations
+    # so we need to convert any
+    # data into string for input
+    stdout.write(str(summation))
+ 
+# call the main method
+if __name__ == "__main__":
+    main()    
