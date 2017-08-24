defc0530bc5330c7952816f12888880b22a0d10a
Bin Liu <bliu@suse.com>
2017-08-24 10:51:34 +0800

    copyreg

---
+++ python/copyreg-example.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+
+import copyreg, copy, pickle
+
+class C(object):
+    def __init__(self, a):
+        self.a = a
+
+def pickle_c(c):
+    print("pickling a C instance...")
+    return C, (c.a,)
+
+copyreg.pickle(C, pickle_c)
+c = C(1)
+d = copy.copy(c)
+print(d)
+
+p = pickle.dumps(c)
+print(p)
