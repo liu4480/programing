9b56808e9842512c364a05b77cc578a81099b645
Bin Liu <bliu@suse.com>
2017-08-24 14:43:18 +0800

    sep parameter in print()

---
+++ python/sep-parameter-print.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+print("G", 'F', 'G', sep='')
+print("09", "12", "2016", sep='-')
+print('pratik','geeksforgeeks', sep='@')
+
+print('G','F', sep='', end='')
+print('G')
+#n provides new line after printing the year
+print('09','12', sep='-', end='-2016\n')
+ 
+print('prtk','agarwal', sep='', end='@')
+print('geeksforgeeks')
