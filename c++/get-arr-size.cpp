39ca8e830550d6f643f9b2ecbd9fdb8eda29b2c2
Bin Liu <bliu@suse.com>
2017-08-30 15:04:55 +0800

    another method to get arr size

--- c++/get-arr-size.cpp
+++ c++/get-arr-size.cpp
@@ -2,10 +2,17 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+ 
+template <typename T, size_t n>
+void findSize(T (&arr)[n])
+{
+    cout << sizeof(T) * n << endl;
+}
+ 
 int main()
 {
     int arr[] = {1, 2 ,3 ,4 ,5, 6};
     int size = *(&arr + 1) - arr;
     cout << "Number of elements in arr[] is " << size << endl;
+    findSize(arr);
 }
